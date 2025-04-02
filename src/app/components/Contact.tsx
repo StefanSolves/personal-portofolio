3700ffd38ccaefc17703c78972233cf7a39d6c86
Stefan <stefansolves@mac.home>
2025-04-02 11:16:41 +0100

    update for deployment

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -42,7 +42,7 @@ const Contact = () => {
               <textarea
                 id="message"
                 name="message"
-                rows="5"
+                rows={5}
                 className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-400"
                 placeholder="Type your message here..."
                 required
